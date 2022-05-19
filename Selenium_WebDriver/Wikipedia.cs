c3c3bdfc331ff99b45591192372f83867b6c3382
Ralisa-Djurkova <ralitsadj@gmail.com>
2022-05-19 10:22:12 +0300

    Add project files.

---
+++ Selenium_WebDriver/Wikipedia.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+driver.Url = "https://www.wikipedia.org/";
+
+var serschField = driver.FindElement(By.Id("searchInput"));
+serschField.Click();
+serschField.SendKeys("QA");
+serschField.SendKeys(Keys.Enter);
+
+driver.Quit();
